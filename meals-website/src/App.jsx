c7fb2062aba309a468877724923761ec2987d1c0
Sharmake Ibrahim <sharmakeahmedibrahim@gmail.com>
2024-06-24 10:13:21 +1200

    small changes made and added api

--- meals-website/src/App.jsx
+++ meals-website/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect, createContext} from "react";
 import Header from "./Components/Header";
 import "./Scss/header.scss";
 import "./Scss/favorite.scss";
@@ -6,7 +6,32 @@ import Meals from "./Components/Meals";
 import Favorite from "./Components/Favorite";
 import "./Scss/meals.scss";
 
+
+
 function App() {
+
+  const api = "https://www.themealdb.com/api/json/v1/1/search.php?f=a";
+
+  const fetchData = async ()=> {
+    try{
+      const res = await fetch(api);
+      const data = await res.json()
+        console.log(data)
+    } 
+   catch(err) {
+    console.log(err.massage);
+   }
+    
+  }
+
+
+  useEffect( ()=>{
+    fetchData()
+
+
+  },[])
+
+
 
   return (
     <>
