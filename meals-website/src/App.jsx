9b7ac489b4b78637686c852f260ad97184ec1b40
Sharmake Ibrahim <sharmakeahmedibrahim@gmail.com>
2024-06-23 22:58:22 +1200

    created favorites page and its responsive

--- meals-website/src/App.jsx
+++ meals-website/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "./Components/Header";
 import "./Scss/header.scss";
+import "./Scss/favorite.scss";
+import Favorite from "./Components/Favorite";
+
 function App() {
 
   return (
     <>
     <Header/>
-      
+    <Favorite/>
+    
     </>
   )
 }
---
+++ meals-website/src/Components/Favorite.jsx
@@ -0,0 +1,47 @@
+
+
+const Favorite = ()=> {
+    return(
+        <section className="favorities">
+            <p> Favorites</p>
+            <div className="grid">
+
+                <div className="item">
+                    <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=600,545" alt="food" />
+                    <span>remove</span>
+                </div>
+                <div className="item">
+                    <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=600,545" alt="food" />
+                    <span>remove</span>
+                </div>
+                <div className="item">
+                    <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=600,545" alt="food" />
+                    <span>remove</span>
+                </div>
+                <div className="item">
+                    <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=600,545" alt="food" />
+                    <span>remove</span>
+                </div>
+                <div className="item">
+                    <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=600,545" alt="food" />
+                    <span>remove</span>
+                </div>
+                <div className="item">
+                    <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=600,545" alt="food" />
+                    <span>remove</span>
+                </div>
+                <div className="item">
+                    <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=600,545" alt="food" />
+                    <span>remove</span>
+                </div>
+                <div className="item">
+                    <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=600,545" alt="food" />
+                    <span>remove</span>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default Favorite;
